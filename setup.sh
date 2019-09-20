2ff1ab7a2320c9f7163592ffb6a83a5c9b502d28
Prashanth Jayaramu <Prashanth.Jayaramu@ge.com>
2019-09-20 17:23:20 +0530

    added spaces necessary

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 !#/bin/shell
+
 echo " ----- setting up Local -----"
 sudo apt install
 sudo apt-get install
 sudo apt upgrade
+
 echo " ----- setting AWS-CLI -----"
 sudo apt install aws
+
 echo " ----- setting Docker -----"
 sudo apt install docker.io
+
 echo " ----- Pulling Solr:6.6.1 -----"
 sudo docker pull solr:6.6.1
