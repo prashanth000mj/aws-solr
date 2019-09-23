d8b9f12ea3e2eab2a5b9bc700b7c0698c5e1ba69
Prashanth Jayaramu <Prashanth.Jayaramu@ge.com>
2019-09-23 10:52:43 +0530

    added all necessary

--- setup.sh
+++ setup.sh
@@ -3,13 +3,18 @@
 echo " ----- setting up Local -----"
 sudo apt install
 sudo apt-get install
-sudo apt upgrade
+sudo apt update
+sudo apt-get update
+apt-get install -y default-jdk maven unzip
 
 echo " ----- setting AWS-CLI -----"
-sudo apt install aws
+sudo apt-get install awscli
 
 echo " ----- setting Docker -----"
 sudo apt install docker.io
 
 echo " ----- Pulling Solr:6.6.1 -----"
 sudo docker pull solr:6.6.1
+
+echo " ----- Pulling Solr:6.6.1 -----"
+sudo docker run -p 8983:8983 solr:6.6.1
